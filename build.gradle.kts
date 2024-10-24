5c55d6cca200d65bcdb21629bf1673a3c42194b1
jason <saga1654@kakao.com>
2024-10-24 16:11:42 +0900

    @Serializable 플러그인

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "2.0.21"
     id("io.ktor.plugin") version "3.0.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20"
 }
 
 group = "org.jason"
